5a46019f996da6dae2eb8969a05568650db69758
Deniz Shyukryuev <denizztto@gmail.com>
2023-06-26 01:05:51 +0300

    Remove produciton message, add sharp lib

--- src/app/body.tsx
+++ src/app/body.tsx
@@ -14,12 +14,6 @@ export default function LayoutBody(props: PropsWithChildren) {
     <QueryClientProvider client={queryClient}>
       <body className={theme.dark ? "dark" : "light"}>
         <Header />
-        {process.env.NODE_ENV === "production" && (
-          <h1 className="display-large m-5 rounded-theme-sm bg-md-light-tertiary p-2 text-center text-md-light-on-tertiary dark:bg-md-dark-tertiary dark:text-md-dark-on-tertiary">
-            Server is not public yet. <br /> REST API request like starting a
-            test WILL cause an error.
-          </h1>
-        )}
         <main className="m-auto max-w-screen-xl pb-12 pt-8 max-2xl:px-8 max-xl:max-w-[100%]">
           {props.children}
         </main>
